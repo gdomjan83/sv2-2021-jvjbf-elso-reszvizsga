f024a0471e0dba66db48977fa7dacd5624229ca1
J_C <gomjan83@gmail.com>
2021-11-09 15:04:44 +0100

    StringManipulation excercise finished

---
+++ src/main/java/stringmanipulation/StringManipulation.java
@@ -0,0 +1,17 @@
+package stringmanipulation;
+
+public class StringManipulation {
+
+    public String everyEvenCharacter(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i % 2 == 0) {
+                sb.append(s.charAt(i));
+            }
+        }
+        String result = new String(sb);
+
+        return result;
+    }
+}
