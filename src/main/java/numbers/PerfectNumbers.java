9104908b4a07fa2427d3362ef25741adb060cbb8
J_C <gomjan83@gmail.com>
2021-11-09 15:14:31 +0100

    Perfect Numbers excercise finished

---
+++ src/main/java/numbers/PerfectNumbers.java
@@ -0,0 +1,19 @@
+package numbers;
+
+public class PerfectNumbers {
+
+    public boolean isPerfectNumber(int number) {
+        int total = 0;
+
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                total += i;
+            }
+        }
+
+        if (total == number) {
+            return true;
+        }
+        return false;
+    }
+}
